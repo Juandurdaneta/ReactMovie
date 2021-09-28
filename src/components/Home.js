56390a46996fb2ed40b6b9f200b448739f6011ef
Juan Urdaneta <juandurdanet@gmail.com>
2021-09-28 13:33:49 -0400

    Grid Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
-// Component 
-import HeroImage from "./HeroImage"
+// Component
+import HeroImage from "./HeroImage";
+import Grid from "./Grid";
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Hook
@@ -9,21 +10,23 @@ import { useHomeFetch } from "../Hooks/useHomeFetch";
 import noImage from "../images/no_image.jpg";
 
 function Home() {
-  
-  const {state, loading, error} = useHomeFetch();
-
+  const { state, loading, error } = useHomeFetch();
 
   return (
     <>
-      
-      {state.results[0] &&
-      <HeroImage 
-        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-        title={state.results[0].original_title}
-        text={state.results[0].overview}
-      />
-      }
+      {state.results[0] && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      )}
 
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </Grid>
     </>
   );
 }
