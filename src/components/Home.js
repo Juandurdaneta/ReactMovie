939e145717a247c8d8cf1f121e368c05ad4c54cf
Juan Urdaneta <juandurdanet@gmail.com>
2021-09-28 11:24:42 -0400

    HeroImage Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+// Component 
+import HeroImage from "./HeroImage"
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Hook
@@ -13,7 +14,19 @@ function Home() {
 
   console.log(state);
 
-  return <div>homepage</div>;
+  return (
+    <>
+      
+      {state.results[0] &&
+      <HeroImage 
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+        title={state.results[0].original_title}
+        text={state.results[0].overview}
+      />
+      }
+
+    </>
+  );
 }
 
 export default Home;
