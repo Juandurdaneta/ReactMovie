67ba6a36a3ee03634ad58a97806870311c463b9c
Juan Urdaneta <juandurdanet@gmail.com>
2021-10-17 22:16:22 -0400

    Movie Component - Scaffold

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,20 @@
-import react from "react";
+import React from "react";
+// config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+// components
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+// hook
 
-const Movie = () => (<div>Movie</div>);
+// image
+import NoImage from "../images/no_image.jpg";
+
+const Movie = () => {
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
+};
 
 export default Movie;
