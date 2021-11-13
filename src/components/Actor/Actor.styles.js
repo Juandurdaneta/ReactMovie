3aaa8eac58ea0fefaccb5d8dd7335340033b36fd
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2021-11-13 19:41:47 -0400

    Add styles to Actor component
    added styles to the component that's rendering actor's data

--- src/components/Actor/Actor.styles.js
+++ src/components/Actor/Actor.styles.js
@@ -1,5 +1,25 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    color: var(--white);
+    background: var(--darkGrey);
+    border-radius: 20px;
+    text-align: center;
+    padding: 5px;
 
-export const Image = styled.img``;
+    h3{
+        margin: 10px 0 0 0;
+    }
+
+    p{
+        margin: 5px 0;
+    }
+`;
+
+export const Image = styled.img`
+    display: block;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 15px;
+    heigth: 200px;
+`;
