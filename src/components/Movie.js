1ed2760abdbb276e79d27911fd11b6282b9000e8
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2021-12-02 22:47:57 -0400

    Refactoring Movie.js
    Refactoring movie component into class components🤢.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { useParams } from "react-router";
 // config
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
@@ -10,17 +10,57 @@ import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
 import Actor from "./Actor";
 
-// hook
-import { useMovieFetch } from "../Hooks/useMovieFetch";
 // image
 import NoImage from "../images/no_image.jpg";
 
+// api
+import API from '../API';
 
-const Movie = () => {
-  const { movieId } = useParams();
+class Movie extends Component {
+  
+  state = {
+    movie: {},
+    loading: true,
+    error: false
+  }
 
-  const { state: movie, loading, error } = useMovieFetch(movieId);
+   fetchMovie = async() => {
+    const { movieId } = this.props.params;
+    try {
+      
+        this.setState({error: false, loading: true})
 
+        const movie = await API.fetchMovie(movieId);
+        const credits = await API.fetchCredits(movieId);
+        // getting only directors
+        const directors = credits.crew.filter(
+            member => member.job === 'Director'
+        );
+
+        this.setState({
+            movie: {
+            ...movie,
+            actors: credits.cast,
+            directors
+          }, 
+          loading: false
+        })
+
+
+        
+
+    } catch (error) {
+        this.setState({error: true, loading: false});
+    }
+};
+
+  componentDidMount(){
+    this.fetchMovie();
+  }
+
+  render() {
+
+  const { movie, loading, error } = this.state; 
 
   if (loading) return <Spinner />;
   if (error) return <div>Something went wrong...</div>;
@@ -46,6 +86,12 @@ const Movie = () => {
       </Grid>
     </>
   );
+
+  }
+
+  
 };
 
-export default Movie;
+const MovieWithParams = props => <Movie  {...props} params={useParams()} />
+
+export default MovieWithParams;
